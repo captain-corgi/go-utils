d0878aca8e4135f1665e3545a0406185963f2974
Lê Tuấn Anh <letuananh.ttmmt.ctu@gmail.com>
2022-09-01 00:54:37 +0700

    Add more comment

--- random/uuid.go
+++ random/uuid.go
@@ -3,6 +3,8 @@ package random
 import "github.com/google/uuid"
 
 // GoogleUUID return random UUID using google randomize
+//
+//	"github.com/google/uuid"
 func GoogleUUID() string {
 	return uuid.New().String()
 }
